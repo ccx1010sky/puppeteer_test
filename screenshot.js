97178148e87d7af1a91ad334e5d6b2122ce53952
ccx1010sky <ccx1010@gmail.com>
2024-02-18 15:06:36 +0000

    function is not working,using page.evaluate() instead to solve the problem

--- screenshot.js
+++ screenshot.js
@@ -1,8 +1,20 @@
 const puppeteer = require("puppeteer");
+
 puppeteer.launch().then(async (browser) => {
   const page = (await browser.pages())[0];
   await page.setViewport({ width: 1280, height: 1024 });
-  await page.goto("http://www.google.com");
-  await page.screenshot({ path: "google.png" });
+  await page.goto("https://www.google.com");
+//   const xpathResult = await page.$x("//button[contains(., 'I agree')]"); // <- evaluate the xpath expression
+//   if (xpathResult.length > 0) {
+//     await xpathResult[0].click(); // <- clicking on the button
+//   }
+const selector = "//button[contains(., 'Accept all')]";
+await page.evaluate((selector) => {
+  const xpathResult = document.evaluate(selector, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+  const button = xpathResult.singleNodeValue;
+  if (button) button.click();
+}, selector);
+  await page.screenshot({ path: "google-clicked.png" });
   await browser.close();
 });
+
